b8374cabdd59d426aa1da4c8c37365720ea3c4de
Brandon <blorenz@gmail.com>
2012-09-10 10:49:54 -0400

    Better boot

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,9 @@ export GEM_HOME="$VIRTUAL_ENV/gems"
 export GEM_PATH=""
 gem install compass
 gem install compass-less-plugin
-cp project_name/settings/local-dist.py project_name/settings/local.py
+cp {{project_name}}/settings/local-dist.py {{project_name}}/settings/local.py
 compass create -r less foo --using less
 mv foo/sass sass
 rm -rf foo
+chmod 755 manage.py
 
