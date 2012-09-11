e41560d64dd89990bf9bcf6ff46832eeff6201c1
Brandon <blorenz@gmail.com>
2012-09-10 22:57:31 -0400

    common out

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -138,11 +138,11 @@ STATICFILES_FINDERS = (
 )
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.common.CommonMiddleware',
+    #'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'commonware.middleware.FrameOptionsHeader',
+    #'commonware.middleware.FrameOptionsHeader',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
